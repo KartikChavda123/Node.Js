9fcd8f5bd3e332f1725c01f0d802959128a0e4fc
KartikChavda123 <kartikchavda2005@gmail.com>
2024-12-16 14:10:52 +0530

    Projeact

--- Day-5  Express Js  CRUD  Book PR/index.js
+++ Day-5  Express Js  CRUD  Book PR/index.js
@@ -10,9 +10,13 @@ let book = [
     {id: "1", bookName: "Don't Believe Everything You Think", author: "oseph Nguyen ", price: "190" },
     {id: "2", bookName: "The Power of Your Subconscious Mind", author: "Joseph Murphy ", price: "150" },
     {id: "3", bookName: "The Power of A Positive Attitude", author: "Roger Fritz", price: "140" },
-    {id: "4", bookName: "You Can", author: "George Matthew Adams", price: "170" },
+    {id: "4", bookName: "You Can", author: "George Matthew Adams", price: "170",},
     {id: "5", bookName: "Why I Am an Atheist Letters & Jail Diary of Bhagat Singh", author: "Bhagat Singh", price: "180"},
     {id: "6", bookName: "Stop Overthinking", author: "Nick Trenton", price: "160"},
+    {id: "7", bookName: "The Laws of the Spirit World", author: "Khorshed Bhavnagri", price: "250"},
+    {id: "8", bookName: "Coffee Can Investing", author: "Saurabh Mukherjea", price: "390"},
+    {id: "9", bookName: "Black Tornado", author: "Sandeep Unnithan", price: "250"},
+    {id: "10", bookName: "Ahmedabad : A City in the World", author: "Amrita Shah", price: "200"},
 ]
 app.get("/", (req, res) => {
     res.render("index", { book });
